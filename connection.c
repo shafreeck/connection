9e1601dbb8654e71b9a308a9e2f9fb18869ab350
renyongquan <renyongquan@360.cn>
2015-03-03 15:17:58 +0800

    add some comments

--- connection.c
+++ connection.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/* Use a 8x8 matrix to representation the chess board
+ * I did not use N*M just wantting to simplify the codes.
+ * */
 #define NUM 8 
-/*connected by a line*/
+/*connected by a line
+ * Check if two points is connected by a line. This works only x1 == x2 or y1 == y2.
+ * Scan the points from x1 to x2 if y1==y2 and x1<=x2, if all points are zero, then
+ * (x1, y1) is connected to (x2, y2) by a line. As the same, scan the points from y1 to y2
+ * when x1==x2 and y1<=y2.
+ * */
 int line_connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
     int i, barrier, begin, end;
     /*connected by a line*/
@@ -33,7 +41,17 @@ int line_connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
     }
     return 0;
 }
-/*connected by a cross*/
+/*connected by a cross
+ * Check if two points is connected by a cross(or an angle ?).
+ * We draw two lines each in horizontal and vertical at the two
+ * points, and we get two intersection points, like:
+ * 1 0 0 2
+ * 0 0 0 0
+ * 4 0 0 3
+ * If we want to check points 1 and 3, we first get points 2(or 4), and check
+ * points 1, 2 and 2, 3; if they all line connected , then we know that 1, 3 are
+ * cross connected.
+ * */
 int cross_connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
     int x, y;
     x = x1, y = y2;
@@ -49,7 +67,10 @@ int cross_connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
     return 0;
 }
 
-/*is connected ?*/
+/*is connected ?
+ * Two points is connected if they are connected by a line, a cross, or a line and
+ * a cross.
+ * */
 int connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
     int x, y;
     /*connected by line or a cross*/
@@ -57,7 +78,7 @@ int connected(int matrix[NUM][NUM], int x1, int y1, int x2, int y2) {
             cross_connected(matrix, x1, y1, x2, y2)) {
         return 1;
     }
-    /*connected by two crosses*/
+    /*connected by a line and a cross(two crosses)*/
     for (x = 0; x < NUM; ++x) {
         if (matrix[x][y1] == 0 && line_connected(matrix, x, y1, x1, y1) && 
                 cross_connected(matrix, x, y1, x2, y2)) {
